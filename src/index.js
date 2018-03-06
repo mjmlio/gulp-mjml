fc618f5523fad3c5235caa3ba283fd2010422429
Florian Maunier <f.maunier@spi.immo>
2018-03-06 13:42:48 +0100

    Upgrade MJML to 4.0.0

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ module.exports = function mjml (mjmlEngine, options) {
       var render
 
       try {
-        render = mjmlEngine.mjml2html(file.contents.toString(), localOptions)
+        render = mjmlEngine(file.contents.toString(), localOptions)
       } catch (e) {
         this.emit('error', raise(e.message))
         return callback()
